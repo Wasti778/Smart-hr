7e9473f85b386ac2d04b1cbbc2ad3c0475e41508
Wasti778 <hossainwastiafzal@gmail.com>
2025-06-08 00:58:43 +0200

    Create index.js

---
+++ public/src/index.js
@@ -0,0 +1,9 @@
+mkdir -p src
+cat > src/index.js <<EOF
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+EOF
